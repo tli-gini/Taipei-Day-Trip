6e19ccaac0d580e42fba94eb2c5e04a06e547b14
tli-gini <ginitong.fish0524@gmail.com>
2024-06-02 18:53:17 +0800

    API-building

--- app.py
+++ app.py
@@ -1,17 +1,98 @@
-from fastapi import *
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Request, HTTPException,Query,Path
+from fastapi.responses import JSONResponse,FileResponse
+from attraction import attraction_name, attraction_mrt,get_attraction_id, get_mrt_stats
+from decimal import Decimal
+import uvicorn
+import json
+
+
 app=FastAPI()
 
+origins = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
+]
+
+def decimal_default(obj):
+    if isinstance(obj, Decimal):
+        return float(obj)  
+    raise TypeError(f"Object of type '{type(obj).__name__}' is not JSON serializable")
+
 # Static Pages (Never Modify Code in this Block)
-@app.get("/", include_in_schema=False)
-async def index(request: Request):
-	return FileResponse("./static/index.html", media_type="text/html")
-@app.get("/attraction/{id}", include_in_schema=False)
-async def attraction(request: Request, id: int):
-	return FileResponse("./static/attraction.html", media_type="text/html")
-@app.get("/booking", include_in_schema=False)
-async def booking(request: Request):
-	return FileResponse("./static/booking.html", media_type="text/html")
-@app.get("/thankyou", include_in_schema=False)
-async def thankyou(request: Request):
-	return FileResponse("./static/thankyou.html", media_type="text/html")
+# @app.get("/", include_in_schema=False)
+# async def index(request: Request):
+# 	return FileResponse("./static/index.html", media_type="text/html")
+
+# @app.get("/attraction/{id}", include_in_schema=False)
+# async def attraction(request: Request, id: int):
+# 	return FileResponse("./static/attraction.html", media_type="text/html")
+
+@app.get("/api/attraction")
+async def get_attraction(page: int = Query(0, ge=0), keyword: str = Query(None, alias="keyword")):
+    if not keyword:
+        raise HTTPException(status_code=500, detail="Keyword is required")
+
+    try:
+        attractions = attraction_name(keyword) + attraction_mrt(keyword)
+        
+        unique_dicts = {}
+        for d in attractions:
+            unique_dicts[d['id']] = d
+
+        unique_attractions = list(unique_dicts.values())
+        items_per_page = 12
+        start_index = page * items_per_page
+        end_index = start_index + items_per_page
+
+        page_data = unique_attractions[start_index:end_index]
+
+        if len(unique_attractions) > end_index:
+            next_page = page + 1
+        else:
+            next_page = None 
+
+        if page_data:
+            return JSONResponse(content={"nextPage": next_page, "data": page_data}, status_code=200)
+        else:
+            return JSONResponse(content={"nextPage": next_page, "data": []}, status_code=200)
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return JSONResponse(content={"error": True, "message": "Internal Server Error"}, status_code=500)
+
+@app.get("/api/attraction/{attractionId}")
+async def get_attraction_by_id(attractionId: int = Path(..., description="The ID of the attraction to retrieve")):
+    try:
+        attraction = get_attraction_id(attractionId)
+        if not attraction:
+            return JSONResponse(content={"error": True, "message": "Attraction not found"}, status_code=500)
+        if 'images' in attraction and attraction['images']:
+            attraction['images'] = json.loads(attraction['images'])
+        # Serialize using json.dumps to handle Decimals and ensure no unwanted escaping
+        response_content = json.dumps({"data": attraction}, default=decimal_default, ensure_ascii=False)
+        return JSONResponse(content=json.loads(response_content), status_code=200)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return JSONResponse(content={"error": True, "message": "Internal Server Error"}, status_code=500)
+    
+
+@app.get("/api/mrts")
+async def get_mrts():
+    try:
+        mrts = get_mrt_stats()
+        mrt_names = [mrt['mrt'] for mrt in mrts] 
+        return JSONResponse(content={"data": mrt_names}, status_code=200)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")  
+        return JSONResponse(content={"error": True, "message": "Internal Server Error. Please try again later."}, status_code=500)
+
+# @app.get("/booking", include_in_schema=False)
+# async def booking(request: Request):
+# 	return FileResponse("./static/booking.html", media_type="text/html")
+
+# @app.get("/thankyou", include_in_schema=False)
+# async def thankyou(request: Request):
+# 	return FileResponse("./static/thankyou.html", media_type="text/html")
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="localhost", port=8000)
---
+++ attraction.py
@@ -0,0 +1,148 @@
+import json
+import mysql.connector
+import re
+from database import connect_db
+from decimal import Decimal
+
+def insert_attraction(cursor, data):
+    check_sql = "SELECT COUNT(*) FROM information WHERE name = %s AND address = %s"
+    cursor.execute(check_sql, (data[0], data[3]))
+    if cursor.fetchone()[0] == 0: 
+        insert_sql = """
+        INSERT INTO information (name, category, description, address, transport, mrt, lat, lng, images)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        cursor.execute(insert_sql, data)
+    else:
+        print("Record already exists for:", data[0])
+
+def main():
+    with open('./data/taipei-attractions.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        attractions_info = data["result"]["results"]
+        
+        db = connect_db()
+        cursor = db.cursor()
+        
+        try:
+            for attraction in attractions_info:
+                name = attraction["name"]
+                category = attraction["CAT"]
+                description = attraction["description"]
+                address = attraction["address"]
+                transport = attraction["direction"]
+                mrt = attraction["MRT"]
+                lat = float(attraction["latitude"]) if attraction["latitude"] else None
+                lng = float(attraction["longitude"]) if attraction["longitude"] else None
+                img_urls = attraction["file"]
+                normalized_img_urls = re.findall(r'https://[^\s]+?(?:\.jpg|\.JPG|\.png|\.PNG)', img_urls)
+                images_json = json.dumps(normalized_img_urls)
+                print(images_json)
+
+                insert_attraction(cursor, (name, category, description, address, transport, mrt, lat, lng, images_json))
+
+            db.commit()
+        except mysql.connector.Error as err:
+            print("Error: ", err)
+            db.rollback()
+        finally:
+            cursor.close()
+            db.close()
+
+if __name__ == "__main__":
+    main()
+
+
+def attraction_name(name):
+    db = connect_db()
+    cursor = db.cursor(dictionary=True)  
+    try:
+        query = "SELECT * FROM information WHERE name LIKE %s"
+        search_term = f"%{name}%"
+        cursor.execute(query, (search_term,))
+        results = cursor.fetchall()
+        
+        # Decode JSON string to Python list right after fetching
+        for result in results:
+            if result['images']:
+                result['images'] = json.loads(result['images'])
+
+            # Convert Decimal types for JSON serialization, if necessary
+            result['lat'] = str(result['lat']) if isinstance(result['lat'], Decimal) else result['lat']
+            result['lng'] = str(result['lng']) if isinstance(result['lng'], Decimal) else result['lng']
+        
+        return results
+    except mysql.connector.Error as e:
+        print(f"Error querying database: {e}")
+        return None
+    finally:
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
+
+def attraction_mrt(mrt):
+    db = connect_db()
+    cursor = db.cursor(dictionary=True)  
+    try:
+        query = "SELECT * FROM information WHERE mrt = %s"
+        cursor.execute(query, (mrt,)) 
+
+        results = cursor.fetchall()
+        
+        for result in results:
+             if 'images' in result and result['images']:
+                result['images'] = json.loads(result['images'])  # Decode JSON string to list
+
+             if isinstance(result.get('lat'), Decimal):
+                result['lat'] = str(result['lat'])
+             if isinstance(result.get('lng'), Decimal):
+                result['lng'] = str(result['lng'])
+
+        
+        return results
+    except mysql.connector.Error as e:
+        print(f"Error querying database: {e}")
+        return None
+    finally:
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
+
+def get_attraction_id(id):
+    db = connect_db()
+    cursor = db.cursor(dictionary=True)
+    try:
+        query = "SELECT * FROM information WHERE id = %s"
+        cursor.execute(query, (id,))
+        result = cursor.fetchone() 
+        return result
+    except mysql.connector.Error as e:
+        print(f"Database error: {e}")
+        return None
+    finally:
+        cursor.close()
+        db.close()
+
+def get_mrt_stats():
+    db = connect_db()
+    cursor = db.cursor(dictionary=True)
+    try:
+        query = """
+        SELECT mrt, COUNT(*) as attraction_count
+        FROM information
+        WHERE mrt IS NOT NULL
+        GROUP BY mrt
+        ORDER BY attraction_count DESC
+        """
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return results
+    except mysql.connector.Error as e:
+        print(f"Database error: {e}")
+        return []
+    finally:
+        cursor.close()
+        db.close()
+
